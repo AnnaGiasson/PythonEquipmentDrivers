bd3d14a1d42ce52a7239c6f586c5cfc334a57b42
TedKus <ted.kus@me.com>
2021-06-10 10:36:00 -0400

    add get_equip to init

--- pythonequipmentdrivers/__init__.py
+++ pythonequipmentdrivers/__init__.py
@@ -1,6 +1,6 @@
 from .__core import (get_devices_addresses, identify_devices, Scpi_Instrument,
                      VisaIOError, EnvironmentSetup,
-                     initiaize_device)
+                     initiaize_device, get_equip)
 
 
 from . import source
@@ -20,6 +20,7 @@ __all__ = ['Scpi_Instrument', 'EnvironmentSetup',
            'get_devices_addresses', 'identify_devices',
 
            'initiaize_device',
+           'get_equip',
 
            'VisaIOError',
 
