07e70f204a9480dce20e086fd31e201a12813ff4
Giasson <agiasson@vicorpower.vicr.com>
2022-06-24 09:05:33 -0400

    Finshed Updating CaliforniaInstruments_CSW5550 to new VisaResource methods - adding type hinting information

--- pythonequipmentdrivers/source/CaliforniaInstruments_CSW5550.py
+++ pythonequipmentdrivers/source/CaliforniaInstruments_CSW5550.py
@@ -1,4 +1,3 @@
-from typing import Union
 from pythonequipmentdrivers import VisaResource
 
 
@@ -21,7 +20,7 @@ class CaliforniaInstruments_CSW5550(VisaResource):
 
         """
 
-        self._resource.write(f"OUTP {1 if state else 0}")
+        self.write_resource(f"OUTP {1 if state else 0}")
 
     def get_state(self) -> bool:
         """
@@ -33,7 +32,7 @@ class CaliforniaInstruments_CSW5550(VisaResource):
             bool: Supply state (True == enabled, False == disabled)
         """
 
-        response = self._resource.query("OUTP?")
+        response = self.query_resource("OUTP?")
         return (int(response) == 1)
 
     def on(self) -> None:
@@ -56,74 +55,56 @@ class CaliforniaInstruments_CSW5550(VisaResource):
 
         self.set_state(False)
 
-    def toggle(self, return_state=False) -> Union[None, bool]:
+    def toggle(self) -> None:
         """
         toggle(return_state=False)
 
         Reverses the current state of the Supply's output
-        If return_state = True the boolean state of the supply after toggle()
-        is executed will be returned.
-
-        Args:
-            return_state (bool, optional): Whether or not to return the state
-                of the supply after changing its state. Defaults to False.
-
-        Returns:
-            Union[None, bool]: If return_state == True returns the Supply state
-                (True == enabled, False == disabled), else returns None
         """
 
         self.set_state(self.get_state() ^ True)
 
-        if return_state:
-            return self.get_state()
-
-    def set_voltage_range(self, voltage_range):
+    def set_voltage_range(self, voltage_range: float) -> None:
         if voltage_range > 156:
-            self._resource.write("VOLT:RANG 312")
+            self.write_resource("VOLT:RANG 312")
         else:
-            self._resource.write("VOLT:RANG 156")
-        return None
+            self.write_resource("VOLT:RANG 156")
 
-    def get_voltage_range(self):
-        return float(self._resource.query("VOLT:RANG?"))
+    def get_voltage_range(self) -> float:
+        return float(self.query_resource("VOLT:RANG?"))
 
-    def set_voltage(self, voltage):
-        self._resource.write(f"VOLT {voltage}")
-        return None
+    def set_voltage(self, voltage: float) -> None:
+        self.write_resource(f"VOLT {voltage}")
 
-    def get_voltage(self):
-        return float(self._resource.query("VOLT?"))
+    def get_voltage(self) -> float:
+        return float(self.query_resource("VOLT?"))
 
-    def set_current(self, current):
-        self._resource.write(f"CURR {current}")
-        return None
+    def set_current(self, current: float) -> None:
+        self.write_resource(f"CURR {current}")
 
-    def get_current(self):
-        return float(self._resource.query("CURR?"))
+    def get_current(self) -> float:
+        return float(self.query_resource("CURR?"))
 
-    def set_frequency(self, frequency):
-        self._resource.write(f"FREQ {frequency}")
-        return None
+    def set_frequency(self, frequency: float) -> None:
+        self.write_resource(f"FREQ {frequency}")
 
-    def get_frequency(self):
-        return float(self._resource.query("FREQ?"))
+    def get_frequency(self) -> float:
+        return float(self.query_resource("FREQ?"))
 
-    def set_phase(self, phase):
-        self._resource.write(f"PHAS {phase}")
-        return None
+    def set_phase(self, phase: float) -> None:
+        self.write_resource(f"PHAS {phase}")
 
-    def get_phase(self):
-        return float(self._resource.query("PHAS?"))
+    def get_phase(self) -> float:
+        return float(self.query_resource("PHAS?"))
 
-    def measure_voltage(self):
-        return float(self._resource.query("MEAS:VOLT?"))
+    def measure_voltage(self) -> float:
+        return float(self.query_resource("MEAS:VOLT?"))
 
-    def measure_current(self):
-        return float(self._resource.query("MEAS:CURR?"))
+    def measure_current(self) -> float:
+        return float(self.query_resource("MEAS:CURR?"))
 
-    def measure_power(self):
-        return float(self._resource.query("MEAS:POW?"))
+    def measure_power(self) -> float:
+        return float(self.query_resource("MEAS:POW?"))
 
-    def measure_frequency(self):
-        return float(self._resource.query("MEAS:FREQ?"))
+    def measure_frequency(self) -> float:
+        return float(self.query_resource("MEAS:FREQ?"))
