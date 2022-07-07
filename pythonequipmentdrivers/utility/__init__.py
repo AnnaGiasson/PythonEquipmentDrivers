fa2a73907a26f79c2c46405846994dc2d4121c66
Anna Giasson <AnnaGraceGiasson@gmail.com>
2022-07-07 17:17:07 -0400

    updated submodule init so Logger can be accessed

--- pythonequipmentdrivers/utility/__init__.py
+++ pythonequipmentdrivers/utility/__init__.py
@@ -1,5 +1,9 @@
-from .data_management import (log_to_csv, dump_data, create_test_log,
-                              dump_array_data)
+from .data_management import (Logger, create_test_log, dump_array_data,
+                              dump_data, log_to_csv)
 
-
-__all__ = ['log_to_csv', 'dump_data', 'create_test_log', 'dump_array_data']
+__all__ = (
+    'log_to_csv',
+    'dump_data',
+    'create_test_log',
+    'dump_array_data',
+    'Logger')
