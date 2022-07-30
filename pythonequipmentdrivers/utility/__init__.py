dfd947f666d5e0194c08ef9089847331acd7ffef
Chris Griffin <cgriffin@vicr.com>
2022-07-30 11:14:38 -0400

    Added AttrDict class (#9)

--- pythonequipmentdrivers/utility/__init__.py
+++ pythonequipmentdrivers/utility/__init__.py
@@ -1,5 +1,6 @@
 from .data_management import (log_data, dump_data, create_test_log,
                               dump_array_data)
+from .data_structures import AttrDict
 
-
-__all__ = ['log_data', 'dump_data', 'create_test_log', 'dump_array_data']
+__all__ = ['log_data', 'dump_data',
+           'create_test_log', 'dump_array_data', 'AttrDict']
