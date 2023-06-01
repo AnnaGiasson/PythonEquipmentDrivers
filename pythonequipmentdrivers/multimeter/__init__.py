73b5e19986574ee157c6fd4f5152568bd9845634
Chris Griffin <cgriffin@vicr.com>
2023-06-01 11:12:36 -0400

    Merge master onto dev (#13)
    * Added AttrDict class (#9)
    
    * Added class for the Fluke_8845A (#10)
    
    The Fluke8845A shares same command set as HP_34401A except for a
    Fluke 45 emulation mode command
    
    * changed self.resource.write to self.write_resource

--- pythonequipmentdrivers/multimeter/__init__.py
+++ pythonequipmentdrivers/multimeter/__init__.py
@@ -2,11 +2,12 @@ from .Fluke_45 import Fluke_45
 from .Fluke_DMM import Fluke_DMM
 from .HP_34401A import HP_34401A
 from .Keysight_34461A import Keysight_34461A
-
+from .Fluke_8845A import Fluke_8845A
 
 __all__ = [
-           'Fluke_45',
-           'Fluke_DMM',
-           'HP_34401A',
-           'Keysight_34461A',
-           ]
+    'Fluke_45',
+    'Fluke_DMM',
+    'HP_34401A',
+    'Keysight_34461A',
+    'Fluke_8845A'
+]
