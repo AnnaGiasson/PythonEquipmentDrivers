ca1576420ff35d89cb2adccb04314ceee466d632
cgriffin <cgriffin@vicr.com>
2023-07-20 13:08:49 -0400

    Added methods to complete Fluke45 class set_range, set_local, set_trigger_source, trigger, config,  and set_emulation_mode

--- pythonequipmentdrivers/multimeter/Fluke_45.py
+++ pythonequipmentdrivers/multimeter/Fluke_45.py
@@ -22,19 +22,68 @@ class Fluke_45(VisaResource):
     http://www.ece.ubc.ca/~eng-services/files/manuals/Man_DMM_fluke45.pdf
     """
 
+    valid_modes = ("AAC", "ADC", "VAC", "VDC" "OHMS", "FREQ", "CONT")
+    ranges = (
+        (
+            {"VDC", "VAC"},
+            (
+                ({"M", "F"}, ((0.3, 1), (3, 2), (30, 3), (300, 4), (1000, 5))),
+                ({"S"}, ((0.1, 1), (1, 2), (10, 3), (100, 4), (1000, 5))),
+            ),
+        ),
+        (
+            {"ADC", "AAC"},
+            (
+                ({"M", "F"}, ((0.03, 1), (0.1, 2), (10, 3))),
+                ({"S"}, ((0.01, 1), (0.1, 2), (10, 3))),
+            ),
+        ),
+        (
+            {"OHM"},
+            (
+                (
+                    {"M", "F"},
+                    (
+                        (300, 1),
+                        (3e3, 2),
+                        (3e4, 3),
+                        (3e5, 4),
+                        (3e6, 5),
+                        (3e7, 6),
+                        (3e8, 7),
+                    ),
+                ),
+                (
+                    {"S"},
+                    (
+                        (100, 1),
+                        (1e3, 2),
+                        (1e4, 3),
+                        (1e5, 4),
+                        (1e6, 5),
+                        (1e7, 6),
+                        (1e8, 7),
+                    ),
+                ),
+            ),
+        ),
+        (
+            {"FREQ"},
+            (({"S", "M", "F"}, ((1e3, 1), (1e4, 2), (1e5, 3), (1e6, 4), (1e7, 5))),),
+        ),
+    )
+
     def __init__(self, address: str, **kwargs) -> None:
         super().__init__(address, **kwargs)
-        self.factor = kwargs.get('factor', 1.0)
-        self.valid_modes = ('AAC', 'ADC', 'VAC', 'VDC'
-                            'OHMS', 'FREQ', 'CONT')
+        self.factor = kwargs.get("factor", 1.0)
 
         if "asrl" in address.lower():
             self._is_serial = True
             # self._flush_receive_buffer()
             visa_resource = self._get_visa_resource()
             visa_resource.flush(BufferOperation.discard_receive_buffer)
-            visa_resource.write_termination = '\r\n'
-            visa_resource.read_termination = '\r\n'
+            visa_resource.write_termination = "\r\n"
+            visa_resource.read_termination = "\r\n"
         else:
             self._is_serial = False
 
@@ -74,7 +123,7 @@ class Fluke_45(VisaResource):
     def query_resource(self, message: str, **kwargs) -> str:
         """
         Fluke45 specific query_resource function
-        takes care of serial response if the device uses serial       
+        takes care of serial response if the device uses serial
         """
         # TODO: an incorrect command will cause the serial response to end up in
         # response then the read_resource() will time out, need to handle better
@@ -83,9 +132,9 @@ class Fluke_45(VisaResource):
             _ = self.read_resource()  # to empty the buffer
         return response
 
-    def _measure_signal(self) -> float:
+    def fetch_data(self) -> float:
         """
-        _measure_signal()
+        fetch_data()
 
         returns the value of the current measurement selected on the
         multimeter display
@@ -95,9 +144,49 @@ class Fluke_45(VisaResource):
 
         response = self.query_resource("VAL?")
 
-        return self.factor*float(response)
+        return self.factor * float(response)
 
-    def set_range(self, n: int, auto_range: bool = False) -> None:
+    def enable_cmd_emulation_mode(self) -> None:
+        """
+        enable_cmd_emulation_mode()
+
+        For use with a Fluke 8845A. Enables the Fluke 45 command set emulation
+        mode.
+        """
+        self.write("L2")
+
+    def set_local(self):
+        """
+        set_local()
+
+        Set the DMM to local mode
+        """
+        if self._is_serial:
+            # there is a specific serial command
+            self.write("LOCS")
+        else:
+            # use the GPIB method
+            super().set_local()
+
+    def _get_range_number(self, value, reverse_lookup=False):
+        mode = self.get_mode()
+        rate = self.get_rate()
+        for valid_modes, rates_list in self.ranges:
+            if mode not in valid_modes:
+                continue
+            for valid_rates, max_values in rates_list:
+                if rate not in valid_rates:
+                    continue
+                for range_, command in max_values:
+                    if value < range_ and not reverse_lookup:
+                        return command
+                    elif command == value and reverse_lookup:
+                        return range_
+                raise ValueError(f"{value=} is greater than highest range")
+
+    def set_range(
+        self, signal_range: float = None, n: int = None, auto_range: bool = False
+    ) -> None:
         """
         set_range(n, auto_range=False)
 
@@ -114,10 +203,13 @@ class Fluke_45(VisaResource):
 
         if auto_range:
             self.write_resource("AUTO")
+            return
+
+        if signal_range is not None:
+            n = self._get_range_number(signal_range)
 
         if n in range(0, 7):
             self.write_resource(f"RANGE {n}")
-
         else:
             raise ValueError("Invalid range option, should be 1-7")
 
@@ -148,7 +240,7 @@ class Fluke_45(VisaResource):
         """
 
         rate = rate.upper()
-        if rate in {'S', 'M', 'F'}:
+        if rate in {"S", "M", "F"}:
             self.write_resource(f"RATE {rate}")
 
         else:
@@ -184,8 +276,10 @@ class Fluke_45(VisaResource):
             self.write_resource(f"FUNC1 {mode}")
 
         else:
-            raise ValueError("Invalid mode option, valid options are: "
-                             + f"{', '.join(self.valid_modes)}")
+            raise ValueError(
+                "Invalid mode option, valid options are: "
+                + f"{', '.join(self.valid_modes)}"
+            )
 
     def get_mode(self) -> str:
         """
@@ -213,9 +307,9 @@ class Fluke_45(VisaResource):
         set_mode method.
         """
 
-        if self.get_mode() != 'VDC':
+        if self.get_mode() != "VDC":
             raise IOError("Multimeter is not configured to measure voltage")
-        return self._measure_signal()
+        return self.fetch_data()
 
     def measure_voltage_rms(self) -> float:
         """
@@ -229,9 +323,9 @@ class Fluke_45(VisaResource):
         set_mode method.
         """
 
-        if self.get_mode() != 'VAC':
+        if self.get_mode() != "VAC":
             raise IOError("Multimeter is not configured to measure AC voltage")
-        return self._measure_signal()
+        return self.fetch_data()
 
     def measure_current(self) -> float:
         """
@@ -245,9 +339,9 @@ class Fluke_45(VisaResource):
         mode with the set_mode method.
         """
 
-        if self.get_mode() != 'ADC':
+        if self.get_mode() != "ADC":
             raise IOError("Multimeter is not configured to measure current")
-        return self._measure_signal()
+        return self.fetch_data()
 
     def measure_current_rms(self) -> float:
         """
@@ -261,9 +355,9 @@ class Fluke_45(VisaResource):
         mode with the set_mode method.
         """
 
-        if self.get_mode() != 'AAC':
+        if self.get_mode() != "AAC":
             raise IOError("Multimeter is not configured to measure AC current")
-        return self._measure_signal()
+        return self.fetch_data()
 
     def measure_resistance(self) -> float:
         """
@@ -277,9 +371,9 @@ class Fluke_45(VisaResource):
         set_mode method.
         """
 
-        if self.get_mode() != 'OHMS':
+        if self.get_mode() != "OHMS":
             raise IOError("Multimeter is not configured to measure resistance")
-        return self._measure_signal()
+        return self.fetch_data()
 
     def measure_frequency(self) -> float:
         """
@@ -293,6 +387,51 @@ class Fluke_45(VisaResource):
         set_mode method.
         """
 
-        if self.get_mode() != 'FREQ':
+        if self.get_mode() != "FREQ":
             raise IOError("Multimeter is not configured to measure frequency")
-        return self._measure_signal()
+        return self.fetch_data()
+
+    def set_trigger_source(self, trigger: str) -> None:
+        """
+        set_trigger_source(trigger)
+
+        Configure the meter trigger source
+
+        source (str): { INTernal or EXTernal }
+        """
+        trigger_type_num = 2 if "ext" in trigger.lower() else 1
+        self.write(f"TRIGGER {trigger_type_num}")
+
+    def trigger(self) -> None:
+        """
+        trigger()
+
+        Send the trigger commmand
+        """
+        self.instrument.write("*TRG")
+
+    def config(
+        self, mode: str, rate: str, signal_range: float = None, range_n: int = None
+    ):
+        """
+        set_mode_adv(mode, range_n, rate)
+
+        A one stop shop to configure the most common operating parameters
+
+        Args:
+            mode (str): type of measurement to be done
+                valid modes are 'AAC', 'ADC','VAC', 'VDC','OHMS', 'FREQ', 'CONT'
+                which correspond to AC current, DC current, AV voltage, DC voltage,
+                resistence, frequency, and continuity respectively (not case
+                sensitive)
+            range_n (float, optional): Set the current range setting used for measurements.
+                valid settings are the integers 1 through 7, meaning of the index
+                depends on which measurement is being performed.
+            signal_range (float, optional): measurement range. Defaults to 'auto'
+            rate (str): speed of sampling
+                valid options are 'S','M', or 'F' for slow, medium, and fast
+                respectively (not case sensitive)
+        """
+        self.set_mode(mode)
+        self.set_range(n=range_n, signal_range=signal_range)
+        self.set_rate(rate)
