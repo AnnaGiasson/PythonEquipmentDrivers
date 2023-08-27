3440553f11d4727060ac1f9d49ee90f04839544b
Chris Griffin <cmg9329@gmail.com>
2023-08-27 18:28:36 -0400

    Fixed linux compatbility issue
    linux can now install and use library except for
    Bode100 due to its dependency on win32com

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import find_packages, setup
 
-setup(name='pythonequipmentdrivers',
-      version='2.0.0',
-      description="""
+setup(
+    name="pythonequipmentdrivers",
+    version="2.0.0",
+    description="""
                   A library of software drivers to interface with various
                   pieces of test instrumentation
                   """,
-      url='https://github.com/AnnaGiasson/PythonEquipmentDrivers',
-      author='Anna Giasson',
-      author_email='AnnaGraceGiasson@GMail.com', license='MIT',
-      packages=find_packages(), zip_safe=False,
-      classifiers=[
-                   'Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'Programming Language :: Python'
-                   ],
-      install_requires=[
-                        'pyvisa',
-                        'numpy',
-                        'pypiwin32',
-                        ],
-      )
+    url="https://github.com/AnnaGiasson/PythonEquipmentDrivers",
+    author="Anna Giasson",
+    author_email="AnnaGraceGiasson@GMail.com",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        "pyvisa",
+        "numpy",
+    ],
+)
