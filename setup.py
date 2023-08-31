c85f8b5f33d10be5776b634062a2f90321f69cd3
Chris Griffin <cmg9329@gmail.com>
2023-08-31 12:23:31 -0400

    Update setup.py with new version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pythonequipmentdrivers",
-    version="2.0.0",
+    version="2.0.1",
     description="""
                   A library of software drivers to interface with various
                   pieces of test instrumentation
