0ffe589fcd9e5a31a03c56c7db1d56e89de6a42d
cgriffin <cgriffin@vicr.com>
2024-07-22 13:08:07 -0400

    Updated the package test to check all items listed in pythonequipment.__all__

--- tests/package_test.py
+++ tests/package_test.py
@@ -4,19 +4,12 @@ from importlib import import_module
 
 class pythonequipmentdrivers_TestCase(unittest.TestCase):
     def test_package_import(self):
-        expected_sub_packages = [
-            "daq",
-            "functiongenerator",
-            "multimeter",
-            "networkanalyzer",
-            "oscilloscope",
-            "powermeter",
-            "sink",
-            "source",
-        ]
+        """
+        check that all attrs expected to be exposed at the package level exist
+        """
 
         package = import_module("pythonequipmentdrivers")
 
-        for sub_package in expected_sub_packages:
+        for sub_package in package.__all__:
             with self.subTest(sub_package=sub_package):
                 self.assertTrue(hasattr(package, sub_package))
