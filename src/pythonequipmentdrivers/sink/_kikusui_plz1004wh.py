84ff42863f21ad3755c145481932e2e3f4099ce9
cgriffin <cgriffin@vicr.com>
2024-08-07 14:19:15 -0400

    Implemented fast sequence setting method

--- src/pythonequipmentdrivers/sink/_kikusui_plz1004wh.py
+++ src/pythonequipmentdrivers/sink/_kikusui_plz1004wh.py
@@ -1,6 +1,60 @@
 from typing import Union
+from dataclasses import dataclass
+import itertools
 
 from ..core import VisaResource
+
+
+@dataclass
+class SequenceStep:
+    current: float
+    trigger: bool
+
+
+@dataclass
+class PulseSeqConfig:
+    """
+    A class that holds the configuration for a load pulse sequence
+    Args:
+        pulse_current (float): current setting for the pulse in amps
+        pulse_width (float): width of the pulse in seconds, will be rounded to a
+            multiple of time_base
+        trig_delay (float): delay seconds from the beginning of the pulse to when a
+            trigger pulse shoud be emitted
+        time_base (float): time duration in seconds of a single step in the sequence
+        initial_idle_time (float): time to remain at idle_current before the pulse.
+            Provides time for the load to turn on and stabilize in order to provide a
+            consistent pulse profile.
+        idle_current (float): current setting to go to before and after the pulse, amps
+
+    """
+
+    pulse_current: float
+    pulse_width: float
+    trig_delay: float
+    time_base: float = 1e-3
+    initial_idle_time: float = 10e-3
+    idle_current: float = 0.0
+
+    def __post_init__(self):
+        END_IDLE_TIME = 1e-3
+        MAX_NUM_STEPS = 1024
+        MIN_TIME_BASE = 100e-6
+        MAX_TIME_BASE = 100e-3
+
+        if self.time_base > MAX_TIME_BASE or self.time_base < MIN_TIME_BASE:
+            raise ValueError(
+                f"time_base is outside of valid range ({MIN_TIME_BASE} - {MAX_TIME_BASE})"
+            )
+
+        total_sequence_length = (
+            self.initial_idle_time + self.pulse_width + END_IDLE_TIME
+        )
+        if total_sequence_length / self.time_base > MAX_NUM_STEPS:
+            min_time_base = total_sequence_length / MAX_NUM_STEPS
+            raise ValueError(
+                f"sequence requires too many steps, increase time base > {min_time_base}s"
+            )
 
 
 class Kikusui_PLZ1004WH(VisaResource):  # 1 kW
@@ -376,3 +430,64 @@ class Kikusui_PLZ1004WH(VisaResource):  # 1 kW
 
         response = self.query_resource("MEAS:POW?")
         return float(response)
+
+    def configure_sequence(
+        self,
+        steps: list[SequenceStep],
+        current_range: str = "HIGH",
+        step_size: float = 1e-3,
+        initialize: bool = True,
+    ):
+        MIN_STEP_SIZE = 100e-6
+        MAX_STEP_SIZE = 100e-3
+        VALID_RANGES = {"LOW", "MEDIUM", "MED", "HIGH"}
+        MAX_SEQ_LENGTH = 1024
+
+        sequence_len = len(steps)
+
+        if sequence_len > MAX_SEQ_LENGTH:
+            raise ValueError(f"sequence length is {sequence_len} > {MAX_SEQ_LENGTH=}")
+        if not current_range.upper() in VALID_RANGES:
+            raise ValueError(f"{current_range=} is not in {VALID_RANGES=}")
+        if step_size > MAX_STEP_SIZE or step_size < MIN_STEP_SIZE:
+            raise ValueError(f"step_size must be <{MAX_STEP_SIZE} and >{MIN_STEP_SIZE}")
+
+        if initialize:
+            # fast sequence requires 11, select it for editing
+            self.write_resource("prog:name 11")
+            # set sequence to fast CC mode
+            self.write_resource("prog:mode fcc")
+            # run the sequence just once
+            self.write_resource("prog:loop 1")
+            # set the step length
+            self.write_resource(f"prog:fsp:time {step_size}")
+            # set the current range for the sequence
+            self.write_resource(f"prog:cran {current_range}")
+            # set the sequence length
+            self.write_resource(f"prog:fsp:end {sequence_len}")
+
+        # Write the sequence of currents in chunks of 8 using prog:fsp:edit:wave
+        # to reduce number of transactions. Write individual steps only when
+        # a trigger pulse is needed
+        for steps_chunk, first_step_idx in zip(
+            itertools.batched(steps, 8),
+            itertools.count(start=1, step=8),
+        ):
+            currents = [step.current for step in steps_chunk]
+            # pad the chunk of currents up to 8 with 0s
+            currents += (8 - len(currents)) * [0]
+            # write the chunk of currents
+            self.write_resource(
+                f"prog:fsp:edit:wave {first_step_idx},"
+                + ",".join(str(c) for c in currents)
+            )
+
+            # set the steps that have triggers
+            for offset, step in enumerate(steps_chunk):
+                if not step.trigger:
+                    continue
+                step_idx = first_step_idx + offset
+                # store the current of the step
+                curr = self.query_resource(f"prog:fsp:edit? {step_idx}")
+                # write the step with a trigger
+                self.write_resource(f"prog:fsp:edit {step_idx},{curr},1")
