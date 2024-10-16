761e91261872d89316fd0f27e6b9f11bdadbf65f
TedKus <ted.kus@me.com>
2024-10-16 10:36:43 -0400

    feat(source): add BK 9140 source

---
+++ pythonequipmentdrivers/source/BKPrecision_9140.py
@@ -0,0 +1,18 @@
+from .Keithley_2231A import Keithley_2231A
+
+
+# acts as an alias of Keithley_2231A
+class BKPrecision_9140(Keithley_2231A):
+    """
+    BKPrecision_9140(address)
+
+    address : str, address of the connected power supply
+
+    object for accessing basic functionallity of the B&K Precision DC supply
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
