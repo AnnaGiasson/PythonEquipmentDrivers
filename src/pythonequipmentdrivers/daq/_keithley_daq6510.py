fb0a9bbfe747a6096db5293cb993137f5783f827
Anna Giasson <AnnaGraceGiasson@GMail.com>
2025-08-21 09:26:41 -0400

    Added DAQ methods to: - configure channels (basic settings) - set up a scan of multiple channels - run the scan - get status of the scan - fetch scan data

--- src/pythonequipmentdrivers/daq/_keithley_daq6510.py
+++ src/pythonequipmentdrivers/daq/_keithley_daq6510.py
@@ -1,5 +1,6 @@
 
 from enum import Enum
+from itertools import batched
 from ..core import VisaResource
 
 class Measurement_Type(Enum):
@@ -30,7 +31,16 @@ class Keithley_DAQ6510(VisaResource):
 
         self.write_resource('*LANG SCPI')
 
-    def get_status(self) -> list[str]:
+    def clear_registers(self) -> None:
+        self.reset()
+        self.clear()
+        self.write_resource('STATus:PRESet')  # Clears event registers
+        self.clear_buffer()
+
+    def clear_buffer(self) -> None:
+        self.write_resource('TRAC:CLE')
+
+    def get_status(self) -> list[str]:  # TODO: Validate response under different status flags
         status_reg = int(self.query_resource('STAT:OPER:COND?'))
 
         bit_decoding = (
@@ -51,5 +61,73 @@ class Keithley_DAQ6510(VisaResource):
 
         return status_flags
 
-    def set_measurement_type(self, channel: int, type: Measurement_Type) -> None:
-        self.write_resource(f'SENSe:FUNCtion "{type.value}", (@{channel})')
+    # def set_measurement_type(self, channel: int, type: Measurement_Type) -> None:
+    #     self.write_resource(f'SENSe:FUNCtion "{type.value}", (@{channel})')
+
+    def select_function(self, function: str, *channels:int) -> None:  # TODO: update function arg to use Enum
+        '''
+        :param function: { VOLTage[:DC] | VOLTage:AC | CURRent[:DC] | CURRent:AC
+                            | RESistance | FRESistance | TEMPerature | FREQuency
+                            | PERiod | CONTinuity }
+        :type function: string
+        :param channel: simple channel 101 or range of channels (@101:120)
+        :type channel: string
+        '''
+        self.write_resource(f'FUNC "{function}", (@{",".join(map(str, channels))})')
+
+    def set_scan_channels(self, *channels: int) -> None:
+        '''
+        :param channel: simple channel 101 or range of channels (@101:120)
+        '''
+        self.write_resource(f'ROUT:SCAN (@{",".join(map(str, channels))})')
+
+    def run_scan(self) -> None:
+        self.write_resource('INIT')
+
+    def set_scan_count(self, count:int) -> None:
+        self.write_resource(f'Route:scan:count:scan {count}')
+
+    def fetch_scan_data(self,
+        n_samples: int,
+        n_channels: int,
+        return_sample_time: bool = False
+    ) -> dict[int, list[float|list[float]]]:
+        START_INDEX = 1 # min value of 1
+        BUFFER_NAME = "defbuffer1"
+        read_type = 'chan,rel,read' if return_sample_time else 'chan,read'
+        response = self.query_resource(f'trace:data? {START_INDEX}, {n_samples*n_channels}, "{BUFFER_NAME}", {read_type}')
+
+        batched_data = batched(
+            response.split(','),
+            3 if return_sample_time else 2
+        )
+
+        data = {}
+        for meas in batched_data:
+
+            chan = int(meas[0])
+            datum = tuple(map(float, meas[1:])) if return_sample_time else float(meas[1])
+
+            if chan not in data.keys():
+                data[chan] = []
+
+            data[chan].append(datum)
+        return data
+
+
+    def get_scan_status(self) -> dict[str, str|int]:  # TODO: Validate functionallity
+        response = self.query_resource('ROUTe:scan:state?')
+
+        state, scan_count, step_count = response.split(';')
+        scan_count = int(scan_count)
+        step_count = int(step_count)
+
+        return {
+            'state': state,
+            'scan_count': scan_count,
+            'step_count': step_count,
+        }
+
+    # TODO: Add method for adjusting range
+    # TODO: Add method for adjusting aperture time
+    # TODO: Add method for configuring relay settings
