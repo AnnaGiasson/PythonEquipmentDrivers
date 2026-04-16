7ce6339b00227cc6b2b1152d4170ec8663063682
Giorlando <Daniel.Giorlando@infineon.com>
2026-04-16 16:36:18 -0400

    Created a common Electronic Load Protocol to allow for common function calls across different brands of electronic loads.

--- src/pythonequipmentdrivers/sink/__init__.py
+++ src/pythonequipmentdrivers/sink/__init__.py
@@ -1,8 +1,40 @@
+from typing import Protocol
+
 from ._chroma_6310 import Chroma_6310
 from ._chroma_63206a import Chroma_63206A
 from ._chroma_63210a import Chroma_63210A
 from ._chroma_63600 import Chroma_63600
 from ._kikusui_plz1004wh import Kikusui_PLZ1004WH
+
+
+class ElectronicLoad(Protocol):
+    """
+    A protocol defining the minimum functionallity of an electronic load. Can be
+    used for type-hinting, intellisense, or other code-completion tools in more
+    complicated automation applications.
+    """
+
+    def set_current(self, current: float) -> None:
+        """sets output level of the electronic load"""
+
+    def get_current(self) -> float:
+        """returns the output level of the electronic load"""
+
+    def set_state(self, state: bool) -> None:
+        """sets the state of the electronic load's output"""
+
+    def get_state(self) -> bool:
+        """returns the state of the electronic load's output"""
+
+    def on(self) -> None:
+        """sets the state of the electronic load's output to active"""
+
+    def off(self) -> None:
+        """sets the state of the electronic load's output to inactive"""
+
+    def toggle(self) -> None:
+        """reverses the state of the electronic load's output"""
+
 
 __all__ = [
     "Chroma_6310",
